842a2838234dceeb8e545ca3bf743c486a5f64b7
Razzakzafar <abdulrazzak1691@gmail.com>
2024-12-05 19:11:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "local_file" "hyderabad" {
-filename = "hyderabad.txt"
-content = "Welcome to hyderabad."
+resource "local_file" "Terra-Pipeline" {
+filename = "Terra.txt"
+content = "This file has been created for the Terraform task to integrate terrafrom in jenkins using Terraform plugin."
 }
