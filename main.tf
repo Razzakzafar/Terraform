3f5e8aa960a84de73668ea3d42262f3bc5fb5b9a
Razzakzafar <abdulrazzak1691@gmail.com>
2024-12-06 16:56:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,20 @@
-resource "local_file" "Terra-Pipeline" {
-filename = "Terra.txt"
-content = "This file has been created for the Terraform task to integrate terrafrom in jenkins using Terraform plugin."
+resource "local_file" "animals" {
+filename = "raccoon.txt"
+content = "This file (animals) has been created for the Terraform task to integrate terrafrom in jenkins using Terraform plugin."
 }
+
+resource "local_file" "my_pet" {
+  filename = var.file_name[count.index]
+  content  = "I like all kind of pets"
+  count = length(var.file_name)
+}
+
+variable "file_name" {
+    default = [
+        "pets.txt",
+        "cats.txt",
+        "dogs.txt",
+        "chameleon.txt"
+    ]  
+}
+
